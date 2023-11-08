8da235d126babc2d54afbd5ae86b9746725d83a1
excellpepple <excellpepple@gmail.com>
2023-11-08 16:19:49 -0500

    Added example class

---
+++ Source/UnrealSandBox/UnrealSandBoxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealSandBoxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSANDBOX_API AUnrealSandBoxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
