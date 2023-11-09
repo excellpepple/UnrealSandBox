97c7051620d4448d7d8caa6aba6bd37f29d25703
excellpepple <excellpepple@gmail.com>
2023-11-09 10:06:23 -0500

    added mysavegame class

---
+++ Source/UnrealSandBox/Private/MySaveGame.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MySaveGame.h"
+UMySaveGame::UMySaveGame()
+{
+	SaveSlotName = TEXT("TestSaveSlot");
+	UserIndex = 0;
+}
---
+++ Source/UnrealSandBox/Public/MySaveGame.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "MySaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSANDBOX_API UMySaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, Category = Basic)
+	FString PlayerName;
+
+	UPROPERTY(VisibleAnywhere, category = Basic)
+	FString SaveSlotName;
+
+	UPROPERTY(VisibleAnywhere, category = Basic)
+	uint32 UserIndex;
+
+	UMySaveGame();
+};
