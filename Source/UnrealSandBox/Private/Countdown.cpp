59d8e10da5898fc2d9fc35d78f30c794de9c7369
excellpepple <excellpepple@gmail.com>
2023-11-09 22:21:55 -0500

    Created a Countdown Timer

---
+++ Source/UnrealSandBox/Private/Countdown.cpp
@@ -0,0 +1,54 @@
+﻿// "Made By  Excell Pepple "
+
+
+#include "Countdown.h"
+
+
+// Sets default values
+ACountdown::ACountdown()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	CountdownText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("CountdownNumber"));
+	CountdownText->SetHorizontalAlignment(EHTA_Center);
+	CountdownText->SetWorldSize(150.0f);
+	RootComponent = CountdownText;
+	CountdownTime = 3;
+}
+
+// Called when the game starts or when spawned
+void ACountdown::BeginPlay()
+{
+	Super::BeginPlay();
+	UpdateTimerDisplay();
+	GetWorldTimerManager().SetTimer(CountdownTimerHandle, this, &ACountdown::AdvanceTimer, 1.0f, true);
+}
+
+// Called every frame
+void ACountdown::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ACountdown::UpdateTimerDisplay()
+{
+	CountdownText->SetText(FText::AsNumber(FMath::Max(CountdownTime, 0)));
+}
+
+void ACountdown::AdvanceTimer()
+{
+	--CountdownTime;
+	UpdateTimerDisplay();
+	if(CountdownTime < 1)
+	{
+		GetWorldTimerManager().ClearTimer(CountdownTimerHandle);
+		CountdownHasFinished();
+	}
+}
+
+void ACountdown::CountdownHasFinished_Implementation()
+{
+	CountdownText->SetText(FText::FromString("Go!"));
+}
+
+
---
+++ Source/UnrealSandBox/Public/Countdown.h
@@ -0,0 +1,43 @@
+﻿// "Made By  Excell Pepple "
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TextRenderComponent.h"
+#include "Countdown.generated.h"
+
+UCLASS()
+class UNREALSANDBOX_API ACountdown : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACountdown();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//how many seconds to countdown from
+	UPROPERTY(EditAnywhere)
+	int32 CountdownTime;
+	
+	UTextRenderComponent* CountdownText;
+
+	void UpdateTimerDisplay();
+
+	void AdvanceTimer();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void CountdownHasFinished();
+	virtual void CountdownHasFinished_Implementation();
+
+	FTimerHandle CountdownTimerHandle;
+	
+};
