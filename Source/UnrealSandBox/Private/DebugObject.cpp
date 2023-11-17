a17ad792e012f0ebf96184368d1d788601180b58
excellpepple <excellpepple@gmail.com>
2023-11-16 22:48:06 -0500

    added basic debugging functional

---
+++ Source/UnrealSandBox/Private/DebugObject.cpp
@@ -0,0 +1,38 @@
+﻿// "Made By  Excell Pepple "
+
+
+#include "DebugObject.h"
+#include "DrawDebugHelpers.h"
+
+
+// Sets default values
+ADebugObject::ADebugObject()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+}
+
+// Called when the game starts or when spawned
+void ADebugObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GEngine){
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString("Hello World"));
+	}
+	UWorld* world = GetWorld();
+	if(world)
+	{
+		FVector Location = GetActorLocation();
+		DrawDebugCircle(world, Location, 40.f, 24, FColor::Red, true);	
+	}
+	
+}
+
+// Called every frame
+void ADebugObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/UnrealSandBox/Public/DebugObject.h
@@ -0,0 +1,25 @@
+﻿// "Made By  Excell Pepple "
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DebugObject.generated.h"
+
+UCLASS()
+class UNREALSANDBOX_API ADebugObject : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADebugObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
